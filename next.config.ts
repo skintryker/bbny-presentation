763510c4ec3fc1e82a7bb80718b29637c6d6d8a7
Reggae Martins <skintryker@gmail.com>
2026-05-09 21:14:33 -0400

    fix: remove basePath for Vercel/Local compatibility

--- next.config.ts
+++ next.config.ts
@@ -2,8 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: 'export',
-  basePath: '/bbny-presentation',
-  assetPrefix: '/bbny-presentation',
   images: {
     unoptimized: true,
   },
