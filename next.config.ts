d0c04fd9ed5340fc41d002648c2add10be7e5dd1
Reggae Martins <skintryker@gmail.com>
2026-05-09 21:10:18 -0400

    fix: adjust basePath for github pages

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,8 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: 'export',
+  basePath: '/bbny-presentation',
+  assetPrefix: '/bbny-presentation',
   images: {
     unoptimized: true,
   },
